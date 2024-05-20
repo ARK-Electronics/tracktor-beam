3aac191c4539c9d0e5040d4771014198b6714faf
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-05-20 16:38:30 -0600

    clean up, publish target pose

--- src/ArucoTracker.cpp
+++ src/ArucoTracker.cpp
@@ -4,8 +4,9 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
+#include <opencv2/core/quaternion.hpp>
 #include <px4_msgs/msg/distance_sensor.hpp>
-#include <px4_msgs/msg/landing_target_pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 class ArucoTrackerNode : public rclcpp::Node
 {
@@ -18,7 +19,9 @@ private:
 
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _image_sub;
 	rclcpp::Subscription<px4_msgs::msg::DistanceSensor>::SharedPtr _distance_sub;
+
 	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _image_pub;
+	rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr _target_pose_pub;
 	cv::Ptr<cv::aruco::Dictionary> _dictionary;
 	// float _ground_distance = 0.f;
 	float _ground_distance = 1.f;
@@ -29,12 +32,12 @@ private:
 ArucoTrackerNode::ArucoTrackerNode()
 	: Node("aruco_tracker_node")
 {
-	RCLCPP_INFO(this->get_logger(), "ArucoTrackerNode :)");
+	RCLCPP_INFO(this->get_logger(), "Starting ArucoTrackerNode");
 	// Define aruco tag dictionary to use
 	_dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_250);
 
 	// Load camera calibration data from YAML file
-	cv::FileStorage fs("usb_cam_calib.yml", cv::FileStorage::READ);
+	cv::FileStorage fs("/home/jake/code/ark/offboard_ws/usb_cam_calib.yml", cv::FileStorage::READ);
 	if (fs.isOpened()) {
 		fs["camera_matrix"] >> _camera_matrix;
 		fs["distortion_coefficients"] >> _dist_coeffs;
@@ -47,9 +50,6 @@ ArucoTrackerNode::ArucoTrackerNode()
 		RCLCPP_ERROR(this->get_logger(), "Failed to load camera parameters correctly.");
 	}
 
-	// Ensure matrix is of type double
-	_camera_matrix.convertTo(_camera_matrix, CV_64F);
-
 	// Setup publishers and subscribers
 	auto qos = rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 5));
 	qos.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
@@ -60,6 +60,8 @@ ArucoTrackerNode::ArucoTrackerNode()
 		"/fmu/out/distance_sensor", qos, std::bind(&ArucoTrackerNode::distance_sensor_callback, this, std::placeholders::_1));
 	_image_pub = this->create_publisher<sensor_msgs::msg::Image>(
 		"/image_proc", qos);
+	_target_pose_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>(
+		"/target_pose", qos);
 }
 
 void ArucoTrackerNode::distance_sensor_callback(const px4_msgs::msg::DistanceSensor::SharedPtr msg)
@@ -72,42 +74,48 @@ void ArucoTrackerNode::image_callback(const sensor_msgs::msg::Image::SharedPtr m
 	try {
 		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
-		// Calculate poses
 		std::vector<int> ids;
 		std::vector<std::vector<cv::Point2f>> corners;
-		std::vector<cv::Vec3d> rvecs, tvecs;
-
 		cv::aruco::detectMarkers(cv_ptr->image, _dictionary, corners, ids);
+		cv::aruco::drawDetectedMarkers(cv_ptr->image, corners, ids);
 
-		// Array of arrays where each array is a set of marker IDs
-		if (ids.size() > 0) {
+		// Calculate marker Pose and draw axes
+		for (size_t i = 0; i < ids.size(); i++) {
+			float pixel_width = cv::norm(corners[i][0] - corners[i][1]);
+			float marker_size = (pixel_width / _camera_matrix.at<double>(0, 0)) * _ground_distance;
+			if (!std::isnan(marker_size) && !std::isinf(marker_size)) {
+				std::vector<cv::Point3f> objectPoints;
+				float half_size = marker_size / 2.0f;
+				objectPoints.push_back(cv::Point3f(-half_size,  half_size, 0));  // top left
+				objectPoints.push_back(cv::Point3f( half_size,  half_size, 0));  // top right
+				objectPoints.push_back(cv::Point3f( half_size, -half_size, 0));  // bottom right
+				objectPoints.push_back(cv::Point3f(-half_size, -half_size, 0));  // bottom left
 
-			RCLCPP_INFO(this->get_logger(), "drawing detected markers");
-			cv::aruco::drawDetectedMarkers(cv_ptr->image, corners, ids);
+				cv::Vec3d rvec, tvec;
+				cv::solvePnP(objectPoints, corners[i], _camera_matrix, _dist_coeffs, rvec, tvec);
+				cv::aruco::drawAxis(cv_ptr->image, _camera_matrix, _dist_coeffs, rvec, tvec, marker_size);
 
-			for (size_t i = 0; i < ids.size(); i++) {
-				float pixel_width = cv::norm(corners[i][0] - corners[i][1]);
-				float marker_size = (pixel_width / _camera_matrix.at<double>(0, 0)) * _ground_distance;
+                // Publish target pose
+                geometry_msgs::msg::PoseStamped target_pose;
+                target_pose.header.stamp = msg->header.stamp;
+                target_pose.header.frame_id = "camera_frame"; // TODO: frame_id
+                target_pose.pose.position.x = tvec[0];
+                target_pose.pose.position.y = tvec[1];
+                target_pose.pose.position.z = tvec[2];
 
-				if (!std::isnan(marker_size) && !std::isinf(marker_size)) {
-					RCLCPP_INFO(this->get_logger(), "marker_size %f", marker_size);
-					std::vector<cv::Point3f> objectPoints;
-					float halfSize = marker_size / 2.0f;
-					objectPoints.push_back(cv::Point3f(-halfSize,  halfSize, 0));  // top left
-					objectPoints.push_back(cv::Point3f( halfSize,  halfSize, 0));  // top right
-					objectPoints.push_back(cv::Point3f( halfSize, -halfSize, 0));  // bottom right
-					objectPoints.push_back(cv::Point3f(-halfSize, -halfSize, 0));  // bottom left
+                cv::Mat rot_mat;
+                cv::Rodrigues(rvec, rot_mat);
+                cv::Quatd quat(rot_mat);
+                target_pose.pose.orientation.x = quat.x;
+                target_pose.pose.orientation.y = quat.y;
+                target_pose.pose.orientation.z = quat.z;
+                target_pose.pose.orientation.w = quat.w;
 
-					cv::Vec3d rvec, tvec;
-					RCLCPP_INFO(this->get_logger(), "solvePnP");
-					cv::solvePnP(objectPoints, corners[i], _camera_matrix, _dist_coeffs, rvec, tvec);
-
-					RCLCPP_INFO(this->get_logger(), "drawing axis");
-					cv::aruco::drawAxis(cv_ptr->image, _camera_matrix, _dist_coeffs, rvec, tvec, marker_size);
-				}
+                _target_pose_pub->publish(target_pose);
 			}
 		}
 
+		// Publish image
 		cv_bridge::CvImage out_msg;
 		out_msg.header = msg->header;
 		out_msg.encoding = sensor_msgs::image_encodings::BGR8;
